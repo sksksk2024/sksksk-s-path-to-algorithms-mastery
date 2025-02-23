b874f6881409df92bd7330dcb7d8f6285b6fb94a
Alex <cota8091@gmail.com>
2025-02-23 21:00:28 +0200

    Stacks Done!

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,34 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+// function reverseStringStack(str) {
+//   const revStr = new Stack();
+//   const revArr = [];
+
+//   for (const char of str) {
+//     revStr.push(char);
+//   }
+
+//   while (!revStr.isEmpty()) {
+//     revArr.push(revStr.pop());
+//   }
+
+//   return revArr.join('');
+// }
+
+function reverseStringStack(str) {
+  const stack = new Stack();
+
+  for (let i = 0; i < str.length; ++i) {
+    stack.push(str[i]);
+  }
+
+  let reversedStr = [];
+
+  while (!stack.isEmpty()) {
+    reversedStr += stack.pop();
+  }
+
+  return reversedStr;
+}
 
 module.exports = reverseStringStack;
--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,19 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  const queue = new Queue();
+
+  for (let i = str.length - 1; i >= 0; --i) {
+    queue.enqueue(str[i]);
+  }
+
+  let reveresedString = '';
+
+  while (!queue.isEmpty()) {
+    reveresedString += queue.dequeue();
+  }
+
+  return reveresedString;
+};
 
 module.exports = reverseStringWithQueue;
